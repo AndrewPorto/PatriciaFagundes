343d037acaef098d53b9631768c7ba15450d5bd3
AndrewPorto <andrewluizporto@hotmail.com>
2023-04-29 14:40:53 -0300

    iniciando aplicacao

---
+++ script.js
@@ -0,0 +1,15 @@
+ScrollReveal({ 
+    reset: true,
+    duration: 1000, 
+    distance: '80px',
+    delay: 150
+});
+
+ScrollReveal().reveal('.div-text', { origin: 'top' });
+ScrollReveal().reveal('.div-vantagens', { origin: 'left' });
+ScrollReveal().reveal('.title', { origin: 'top' });
+ScrollReveal().reveal('.div-coluns', { origin: 'top' });
+ScrollReveal().reveal('.atendimento-h2', { origin: 'left' });
+ScrollReveal().reveal('.tipos-atendimentos', { origin: 'left' });
+ScrollReveal().reveal('.ajuda-h2', { origin: 'top' });
+ScrollReveal().reveal('.container-list', { origin: 'top' });
